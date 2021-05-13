20f9030ee17fa4e467609194c50caa5d980e2751
bajergis <64094239+bajergis@users.noreply.github.com>
2021-05-13 21:59:53 +0200

    improved camera movement

--- Assets/Scripts/MANewCameraMovement.cs
+++ Assets/Scripts/MANewCameraMovement.cs
@@ -7,14 +7,13 @@ public class MANewCameraMovement : MonoBehaviour
 
     public float mouseSensitivity = 100f;
 
+    public Camera mainCamera;
     public Transform playerBody;
 
     private Vector3 defaultCameraPosition;
 
     float xRotation = 11f;
     float yRotation = 270f;
-
-    private Time timer;
 
     // Start is called before the first frame update
     void Start()
@@ -29,11 +28,12 @@ public class MANewCameraMovement : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        this.mainCamera.transform.position = Vector3.Lerp(this.mainCamera.transform.position, this.transform.position, 0.05f);
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -7f, 7f);
         yRotation -= mouseX;
         yRotation = Mathf.Clamp(yRotation, -7f, 7f);
 
-        transform.localRotation = Quaternion.Euler(11f + xRotation, 270f - yRotation, 0f);
+        this.transform.localRotation = Quaternion.Euler(11f + xRotation, 270f - yRotation, 0f);
     }
 }
